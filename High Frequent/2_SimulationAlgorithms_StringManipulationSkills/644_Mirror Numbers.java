d9e6e60d565e79fb451d66dbede44a65ea5e917e
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2017-09-16 16:02:02 -0700

    Create 644_Mirror Numbers.java

---
+++ High Frequent/2_SimulationAlgorithms_StringManipulationSkills/644_Mirror Numbers.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /*
+     * @param num: a string
+     * @return: true if a number is strobogrammatic or false
+     */
+    public boolean isStrobogrammatic(String num) {
+        // write your code here
+        Map<Character, Character> map = new HashMap<>();
+        map.put('0', '0');
+        map.put('1', '1');
+        map.put('8', '8');
+        map.put('6', '9');
+        map.put('9', '6');
+        int left = 0;
+        int right = num.length() - 1;
+        while (left <= right) {
+            if (!map.containsKey(num.charAt(left))) {
+                return false;
+            }
+            if (map.get(num.charAt(left)) != num.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
