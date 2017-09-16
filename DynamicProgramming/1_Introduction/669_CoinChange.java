b908a6f24803e9e84a225121f79388d1ddeca241
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2017-09-16 15:52:53 -0700

    Create 669_CoinChange.java

---
+++ DynamicProgramming/1_Introduction/669_CoinChange.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /*
+     * @param coins: a list of integer
+     * @param amount: a total amount of money amount
+     * @return: the fewest number of coins that you need to make up
+     */
+    public int coinChange(int[] coins, int amount) {
+        // write your code here
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int k = 0; k < coins.length; k++) {
+                if(i >= coins[k] && dp[i - coins[k]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[k]] + 1);
+                }
+            }
+        }
+        if (dp[amount] == Integer.MAX_VALUE) {
+            return -1;
+        } else {
+            return dp[amount];
+        }
+    }
+}
