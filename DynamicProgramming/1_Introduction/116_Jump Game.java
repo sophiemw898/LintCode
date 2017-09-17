42ac6653f1315ef64f4c84a03f2cabde55a702d1
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2017-09-17 11:03:13 -0700

    Create 116_Jump Game.java

---
+++ DynamicProgramming/1_Introduction/116_Jump Game.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /*
+     * @param A: A list of integers
+     * @return: A boolean
+     */
+    public boolean canJump(int[] A) {
+        // write your code here
+        boolean[] dp = new boolean[A.length];
+        dp[0] = true;
+        for (int j = 1; j < A.length; j++) {
+            dp[j] = true;
+            for (int i = 0; i < j; i++) {
+                if (dp[i] && i + A[i] >= j) {
+                    dp[j] = true;
+                    break;
+                }
+                else {
+                    dp[j] = false;
+                }
+            }
+        }
+        return dp[A.length - 1];
+    }
+}
