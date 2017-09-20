967409fddec497f6af95eadc914be0fb92ce473f
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2017-09-20 10:35:04 -0700

    Create 459_Closest Number in Sorted Array.java

---
+++ 1_Algorithm/2_Binary Search/459_Closest Number in Sorted Array.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    public int closestNumber(int[] A, int target) {
+        // Write your code here
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] > target) {
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            }
+        }
+        return (target - A[start]) <= (A[end] - target) ? start : end;
+    }
+}
+O(log n)
