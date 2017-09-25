1b7279b14e0dbe054b490aae9fc3fc9120226e75
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2017-09-25 12:27:05 -0700

    Create 596_Minimum Subtree.java

---
+++ 1_Algorithm/3_BinaryTree-DivideConquer/596_Minimum Subtree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: the root of binary tree
+     * @return: the root of the minimum subtree
+     */
+    private TreeNode res = null;
+    private int min = Integer.MAX_VALUE;
+    public TreeNode findSubtree(TreeNode root) {
+        // if (root == null || root.size() == 0) {
+        //     return res;
+        // }
+        helper(root);
+        return res;
+    }
+    
+    //helper to return the sum of subtree
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = root.val + helper(root.left) + helper(root.right);
+        if (sum <= min) {
+            min = sum;
+            res = root;
+        }
+        return sum;
+    }
+}    
