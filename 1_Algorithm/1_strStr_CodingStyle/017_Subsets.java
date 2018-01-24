6ecaa993b58745bb92b3428189f01619f96e0f92
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-01-24 15:28:52 -0800

    Update 017_Subsets.java

--- 1_Algorithm/1_strStr_CodingStyle/017_Subsets.java
+++ 1_Algorithm/1_strStr_CodingStyle/017_Subsets.java
@@ -1,3 +1,5 @@
+Space O（N）
+
 public class Solution {
     /*
      * @param nums: A set of numbers
@@ -5,27 +7,38 @@ public class Solution {
      */
     public List<List<Integer>> subsets(int[] nums) {
         // write your code here
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            res.add(new ArrayList<Integer>());
-            return res;
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> sebset = new ArrayList<>();
+        
+        if (nums == null) {
+            return results;
         }
-    
-        List<Integer> path = new ArrayList<>();
+        if (nums.length == 0) {
+            results.add(new ArrayList<Integer>());
+            return results;
+        }
+        //Deduplication
         Arrays.sort(nums);
-        
-        helper(nums, path, res, 0);
-        return res;
-        }
-        
-    public void helper(int[] nums, List<Integer> path, List<List<Integer>> res, int pos) {
-        res.add(new ArrayList<>(path));
-        
-        for(int i = pos; i < nums.length; i++) {
-            path.add(nums[i]);
-            helper(nums, path, res, i + 1);
-            path.remove(path.size() - 1);
-        }
-        return;
+        //找到所有【】开头的集合，并加到 results
+        helper(sebset, nums, 0, results);
+        return results;
     } 
+    //递归三要素 
+    //1.递归的定义 
+    //找到所有 subset开头的集合，并加到 results
+    private void helper(List<Integer> subset, int[] nums, int startIndex, List<List<Integer>> results) {
+        //2.递归的拆解 deep copy 
+        results.add(new ArrayList<Integer>(subset));
+        
+        for(int i = startIndex; i < nums.length; i++) {
+            //尝试寻找 subset+ [nums[i]]开头的所有的子集 
+            //[] + [1]
+            //[] + [2]
+            subset.add(nums[i]);
+            helper(subset, nums, i + 1, results);
+            //backtracking
+            subset.remove(subset.size() - 1);
+        }
+        //3.递归的出口 
+    }
 }
