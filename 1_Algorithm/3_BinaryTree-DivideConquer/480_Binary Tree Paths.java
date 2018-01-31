8673fe48136ea2139d3de97ac3b6c1e013f2664c
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-01-31 11:47:17 -0800

    Update 480_Binary Tree Paths.java

--- 1_Algorithm/3_BinaryTree-DivideConquer/480_Binary Tree Paths.java
+++ 1_Algorithm/3_BinaryTree-DivideConquer/480_Binary Tree Paths.java
@@ -40,3 +40,38 @@ public class Solution {
         return paths;
     }
 }
+
+
+//Traversal
+
+public class Solution {
+    /*
+     * @param root: the root of the binary tree
+     * @return: all root-to-leaf paths
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        // write your code here
+        List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+        helper(root, String.valueOf(root.val), paths);
+        return paths;
+    }
+    
+    private void helper(TreeNode root, String path, List<String> paths) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            paths.add(path);
+            return;
+        }
+        if (root.left != null) {
+            helper(root.left, path + "->" + root.left.val, paths);
+        }
+        if (root.right != null) {
+            helper(root.right, path + "->" + root.right.val, paths);
+        }
+    }
+}
