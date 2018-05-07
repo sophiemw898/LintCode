a6b7d977e9495287307a7482868ba563fa3551fc
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-05-07 12:41:02 -0700

    Create 539_Move Zeroes.java

---
+++ 1_Algorithm/7_TwoPointers/539_Move Zeroes.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param nums: an integer array
+     * @return: nothing
+     */
+    public void moveZeroes(int[] nums) {
+        // write your code here
+        int i = 0, j = 0;
+        while (i < nums.length) {
+            if (nums[i] != 0) {
+                reverse(nums, i, j);
+                j++;
+            }
+            i++;
+        }
+    }
+    
+    private void reverse(int[]nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
