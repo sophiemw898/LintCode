743e7a71de99fe120600c86af61489a69d36adb2
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-05-07 12:52:44 -0700

    Create 521_Remove Duplicate Numbers in Array.java

---
+++ 1_Algorithm/7_TwoPointers/521_Remove Duplicate Numbers in Array.java
@@ -0,0 +1,24 @@
+// O(nlogn) time, O(1) extra space
+public class Solution {
+    /*
+     * @param nums: an array of integers
+     * @return: the number of unique integers
+     */
+    public int deduplication(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+        
+        int len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[len] != nums[i]) {
+                len++;
+                nums[len] = nums[i];
+            }
+        }
+        return len + 1;
+    }
+}
