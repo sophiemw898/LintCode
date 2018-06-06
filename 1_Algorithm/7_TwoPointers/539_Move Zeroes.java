e93509bbd05dcea018d15b6d0e0cf3a5ac7639a7
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-06-06 11:20:07 -0700

    Update 539_Move Zeroes.java

--- 1_Algorithm/7_TwoPointers/539_Move Zeroes.java
+++ 1_Algorithm/7_TwoPointers/539_Move Zeroes.java
@@ -3,9 +3,11 @@ public class Solution {
      * @param nums: an integer array
      * @return: nothing
      */
+    
     public void moveZeroes(int[] nums) {
         // write your code here
         int i = 0, j = 0;
+        //j point to the first 0
         while (i < nums.length) {
             if (nums[i] != 0) {
                 reverse(nums, i, j);
