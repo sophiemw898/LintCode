660220dc7e0563f415f2df30dd1f44cedb6f8a55
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-06-12 14:12:21 -0700

    Create 69_Binary Tree Level Order Traversal.java

---
+++ 1_Algorithm/4_BFS/69_Binary Tree Level Order Traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curLevel = new ArrayList<>();
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                curLevel.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            
+            result.add(curLevel);
+        }
+        
+        return result;
+    }
+}
