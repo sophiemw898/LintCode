8c945c8c00461e61949d90fc215cc1b48ebfe1e2
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-06-27 13:23:17 -0700

    Create 68_Binary Tree Postorder Traversal.java

---
+++ 1_Algorithm/5_Binary Tree - Tree Based DFS/68_Binary Tree Postorder Traversal.java
@@ -0,0 +1,20 @@
+//Recursion
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+             return result;
+        }
+
+        result.addAll(postorderTraversal(root.left));
+        result.addAll(postorderTraversal(root.right));
+        result.add(root.val);
+
+        return result;   
+    }
+}
