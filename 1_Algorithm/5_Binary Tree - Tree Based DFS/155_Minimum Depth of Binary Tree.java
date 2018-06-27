7e26832990213db24ed4bcd894ace84bbf5e41ef
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-06-27 12:32:28 -0700

    Create 155_Minimum Depth of Binary Tree.java

---
+++ 1_Algorithm/5_Binary Tree - Tree Based DFS/155_Minimum Depth of Binary Tree.java
@@ -0,0 +1,39 @@
+public class Solution {
+    /**
+     * @param root: The root of binary tree
+     * @return: An integer
+     */
+    
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.right == null || root.left == null) {
+            return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+        
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
+
+
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return getMin(root);
+    }
+
+    public int getMin(TreeNode root){
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        return Math.min(getMin(root.left), getMin(root.right)) + 1;
+    }
+}
