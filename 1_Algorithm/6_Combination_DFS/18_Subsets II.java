db3348a8142c31658d6e81c1e872a70d711b80af
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-10 12:27:12 -0700

    Create 18_Subsets II.java

---
+++ 1_Algorithm/6_Combination_DFS/18_Subsets II.java
@@ -0,0 +1,45 @@
+public class Solution {
+    /**
+     * @param nums: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        
+        if (nums == null) {
+            return results;
+        }
+        
+        if (nums.length == 0) {
+            results.add(subset);
+            return results;
+        }
+        
+        Arrays.sort(nums);
+        //找到所有【】开头的集合，并加到 results
+        helper(nums, 0, results, subset);
+        return results;
+    }
+    
+    //递归三要素 
+    //1.递归的定义 
+    //找到所有 subset开头的集合，并加到 results
+    private void helper(int[] nums, int startIndex,
+                        List<List<Integer>> results, List<Integer> subset) {
+                            
+                            // 2. 递归的拆解
+                            // deep copy
+                            results.add(new ArrayList<Integer>(subset));
+                            
+                            for (int i = startIndex; i < nums.length; i++) {
+                                //i- 1 没选， 若i同i- 1相同，则同样不选
+                                if (i > startIndex && nums[i] == nums[i - 1]) {
+                                    continue;
+                                }
+                                subset.add(nums[i]);
+                                helper(nums, i + 1, results, subset);
+                                subset.remove(subset.size() - 1);
+                            }
+    }
+}
