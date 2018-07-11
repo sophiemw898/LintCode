79086ed7bbff3f85e6bb2c3d15f77b11ef7d1010
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-11 13:46:10 -0700

    Update 595_Binary Tree Longest Consecutive Sequence.java

--- 1_Algorithm/5_Binary Tree - Tree Based DFS/595_Binary Tree Longest Consecutive Sequence.java
+++ 1_Algorithm/5_Binary Tree - Tree Based DFS/595_Binary Tree Longest Consecutive Sequence.java
@@ -29,3 +29,52 @@ public class Solution {
         return Math.max(length, Math.max(left, right));
     }
 }
+
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: the length of the longest consecutive sequence path
+     */
+    private int length;
+    public int longestConsecutive(TreeNode root) {
+        length = 0;
+        helper(root);
+        return length;
+    }
+    
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int maxSubtree = 1;// at least we have root 
+        int left = helper(root.left);
+        int right = helper(root.right);
+        
+        if (root.left != null && root.left.val == root.val + 1) {
+            maxSubtree = Math.max(maxSubtree, left + 1);
+        }
+        if (root.right != null && root.right.val == root.val + 1) {
+            maxSubtree = Math.max(maxSubtree, right + 1);
+        }
+        
+        if (maxSubtree > length) {
+            length = maxSubtree;
+        }
+        
+        return maxSubtree;
+    }
+}
