d6680c577a5cf27a9692be86ab6d09ea75dad99f
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-11 13:40:23 -0700

    Create 595_Binary Tree Longest Consecutive Sequence.java

---
+++ 1_Algorithm/5_Binary Tree - Tree Based DFS/595_Binary Tree Longest Consecutive Sequence.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: the length of the longest consecutive sequence path
+     */
+    public int longestConsecutive(TreeNode root) {
+        return helper(root, null, 0);
+    }
+    private int helper(TreeNode root, TreeNode parent, int lengthWithoutRoot) {
+        if (root == null) {
+            return 0;
+        }
+        int length = (parent != null && parent.val + 1 == root.val) ? lengthWithoutRoot + 1 : 1;
+        int left = helper(root.left, root, length);
+        int right = helper(root.right, root, length);
+        
+        return Math.max(length, Math.max(left, right));
+    }
+}
