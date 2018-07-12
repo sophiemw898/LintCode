3ec35b5ddd3f46cc12651fe902814135f329013b
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-12 13:12:47 -0700

    Create 88_Lowest Common Ancestor of a Binary Tree.java

---
+++ 1_Algorithm/5_Binary Tree - Tree Based DFS/88_Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of the binary search tree.
+     * @param A: A TreeNode in a Binary.
+     * @param B: A TreeNode in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+        if (root == null || A == root || B == root) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, A, B);
+        TreeNode right = lowestCommonAncestor(root.right, A, B);
+        
+        if (left != null && right != null) {
+            return root;
+        }
+        
+        return right == null ? left : right;
+    }
+}
