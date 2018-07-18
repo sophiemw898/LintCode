564f94f98197790e561d5821fd5f3a0b515a5c4a
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-18 15:54:52 -0700

    Create 111_Climbing Stairs.java

---
+++ 1_Algorithm/10_Dynamic Programming/111_Climbing Stairs.java
@@ -0,0 +1,22 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[n + 1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] =  dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+}
