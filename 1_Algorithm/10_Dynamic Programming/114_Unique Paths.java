192761498a84bbe089a9ab986c7a14b48cfaf76f
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-18 15:33:51 -0700

    Create 114_Unique Paths.java

---
+++ 1_Algorithm/10_Dynamic Programming/114_Unique Paths.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /*
+     * @param m: positive integer (1 <= m <= 100)
+     * @param n: positive integer (1 <= n <= 100)
+     * @return: An integer
+     */
+    public int uniquePaths(int m, int n) {
+        //定义 走到mn可能的走法数
+        int[][] dp = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+}
+O(mn)
