2744294809ef3bd4009cf2d410bb756714ba9790
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-18 14:47:39 -0700

    Update 121_Word Ladder II.java

--- 1_Algorithm/7_Permutation-based - Graph-based DFS/121_Word Ladder II.java
+++ 1_Algorithm/7_Permutation-based - Graph-based DFS/121_Word Ladder II.java
@@ -1,3 +1,87 @@
+//end -> start : bfs    输出所有点距离end 的位置
+//start -> end : dfs 直接输出
+
+public class Solution {
+    /*
+     * @param start: a string
+     * @param end: a string
+     * @param dict: a set of string
+     * @return: a list of lists of string
+     */
+    public List<List<String>> findLadders(String start, String end, Set<String> dict) {
+        List<List<String>> results = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        Map<String, List<String>> graph = new HashMap<>();
+        Map<String, Integer> distance = new HashMap<>();
+        
+        dict.add(start);
+        dict.add(end);
+        
+        bfs(graph, distance, end, start, dict);//输出每个点到end的距离
+        
+        dfs(graph, distance, start, end, results, path);//从start出发dfs
+        
+        return results;
+    }
+    
+    private void dfs(Map<String, List<String>> graph, Map<String, Integer> distance, 
+                String curr, String end, List<List<String>> results, List<String> path) {
+                    path.add(curr);
+                    if (curr.equals(end)) {
+                        results.add(new ArrayList<String>(path));
+                        path.remove(path.size() - 1);
+                        return;
+                    }
+                    for (String nextWord : graph.get(curr)) {
+                        if (distance.containsKey(nextWord) && 
+                            distance.get(nextWord) + 1 == distance.get(curr)) {
+                                dfs(graph, distance, nextWord, end, results, path);
+                            }
+                    }
+                    path.remove(path.size() - 1);
+                }
+    
+    //bfs 同时更新graph和distance 
+    private void bfs(Map<String, List<String>> graph, Map<String, Integer> distance, 
+                String start, String end, Set<String> dict) {
+                    Queue<String> queue = new LinkedList<>();
+                    queue.offer(start);
+                    distance.put(start, 0);
+                    for (String word : dict) {
+                        graph.put(word, new ArrayList<String>());
+                    }
+                    while (!queue.isEmpty()) {
+                        String curr = queue.poll();
+                        for (String nextWord : getNextWords(dict, curr)) {
+                            graph.get(curr).add(nextWord);
+                            if (!distance.containsKey(nextWord)) {
+                                distance.put(nextWord, distance.get(curr) + 1);
+                                queue.offer(nextWord);
+                            }
+                        }
+                    }
+                }
+                
+    private List<String> getNextWords(Set<String> dict, String curr) {
+        List<String> nextWords = new ArrayList<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < curr.length(); i++) {
+                if (curr.charAt(i) == c) {
+                    continue;
+                }
+                String nextWord = curr.substring(0, i) + c + curr.substring(i + 1);
+                if(dict.contains(nextWord)) {
+                    nextWords.add(nextWord);
+                }
+            }
+        }
+        
+        return nextWords;
+    }
+}
+
+
+
 public class Solution {
     /*
      * @param start: a string
