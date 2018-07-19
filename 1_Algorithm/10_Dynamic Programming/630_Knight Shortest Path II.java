eac9e1d08cd052c0d4d4896544398047c3a7e2cd
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-19 15:42:04 -0700

    Create 630_Knight Shortest Path II.java

---
+++ 1_Algorithm/10_Dynamic Programming/630_Knight Shortest Path II.java
@@ -0,0 +1,43 @@
+public class Solution {
+    /**
+     * @param grid: a chessboard included 0 and 1
+     * @return: the shortest path
+     */
+    public int shortestPath2(boolean[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return -1;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        dp[0][0] = 0;
+        
+        //注意计算顺序！！！
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                if (!grid[i][j]) {
+                    if(i >= 1 && j >= 2 && dp[i - 1][j - 2] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 2] + 1);
+                    }
+                    if(i >= 2 && j >= 1 && dp[i - 2][j - 1] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 2][j - 1] + 1);
+                    }
+                    if(i + 1 < m && j >= 2 && dp[i + 1][j - 2] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i + 1][j - 2] + 1);
+                    }
+                    if(i + 2 < m && j >= 1 && dp[i + 2][j - 1] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i + 2][j - 1] + 1);
+                    }
+                }
+            }
+        }
+        
+        return (dp[m - 1][n - 1] == Integer.MAX_VALUE) ? -1 : dp[m - 1][n - 1];
+    }
+}
