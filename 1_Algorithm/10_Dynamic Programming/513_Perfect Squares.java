9565ecf3781d83cfc0a1c32258cb569208860dfe
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-19 12:15:01 -0700

    Create 513_Perfect Squares.java

---
+++ 1_Algorithm/10_Dynamic Programming/513_Perfect Squares.java
@@ -0,0 +1,22 @@
+public class Solution {
+    /**
+     * @param n: a positive integer
+     * @return: An integer
+     */
+    public int numSquares(int n) {
+        int[] dp =  new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        for (int i = 1; i * i <= n ;i++) {
+            dp[i * i] = 1;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j < i; j++) {
+                dp[i] = Math.min(dp[i], (dp[i - j * j] + 1));
+            }
+        }
+        
+        return dp[n];
+    }
+}
