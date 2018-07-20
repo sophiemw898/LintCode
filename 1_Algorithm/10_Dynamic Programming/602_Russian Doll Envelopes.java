2f67b73da8d1ebac0cbe7137247ddb6cd00ccf7e
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-20 14:32:17 -0700

    Update 602_Russian Doll Envelopes.java

--- 1_Algorithm/10_Dynamic Programming/602_Russian Doll Envelopes.java
+++ 1_Algorithm/10_Dynamic Programming/602_Russian Doll Envelopes.java
@@ -1,3 +1,59 @@
+//O(Nlogn) 没自己写
+
+public class Solution {
+    /*
+     * @param envelopes: a number of envelopes with widths and heights
+     * @return: the maximum number of envelopes
+     */
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0){
+            return 0;
+        }
+        Arrays.sort(envelopes, new Comparator<int[]>(){
+            public int compare(int[] e1, int[] e2) {
+                int diff = e1[0] - e2[0];
+                if (diff == 0){
+                    diff = e2[1] - e1[1];
+                }
+                return diff;
+            }
+        });
+        int[][] temp = new int[envelopes.length + 1][2];
+        temp[0][0] = Integer.MIN_VALUE;
+        temp[0][1] = Integer.MIN_VALUE;
+        for (int i = 1; i < temp.length; i++){
+            temp[i][0] = Integer.MAX_VALUE;
+            temp[i][1] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i < envelopes.length; i++){
+            int index = search(envelopes[i], temp);
+            temp[index] = envelopes[i];
+        }
+        for (int j = temp.length - 1; j >= 0; j--){
+            if (temp[j][0] != Integer.MAX_VALUE && temp[j][1] != Integer.MAX_VALUE){
+                return j;
+            }
+        }
+        return 1;
+    }
+    private int search(int[] env, int[][] temp) {
+        int start = 0;
+        int end = temp.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (!isVal(env, temp[mid])){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        return end;
+    }
+    private boolean isVal(int[] env1, int[] env2) {
+        return env1[0] > env2[0] && env1[1] > env2[1];
+    }
+}
+
 //o(n^2) 超时
 public class Solution {
     /*
