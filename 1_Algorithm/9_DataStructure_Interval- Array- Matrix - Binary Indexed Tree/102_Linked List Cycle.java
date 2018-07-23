5189efd6af26d7abf13a90cadde40a4595d20dea
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-23 14:52:02 -0700

    Create 102_Linked List Cycle.java

---
+++ 1_Algorithm/9_DataStructure_Interval- Array- Matrix - Binary Indexed Tree/102_Linked List Cycle.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /*
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        } 
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return true;
+    }
+}
