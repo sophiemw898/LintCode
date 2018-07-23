f420a52f4b1ac4c351232de30bd6c67e800e9649
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-23 14:55:58 -0700

    Create 103_Linked List Cycle II.java

---
+++ 1_Algorithm/9_DataStructure_Interval- Array- Matrix - Binary Indexed Tree/103_Linked List Cycle II.java
@@ -0,0 +1,33 @@
+public class Solution {
+    /*
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins. if there is no cycle, return null
+     */
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        } // Has the cycle and meet 1st time
+        
+        
+        //At this time, slow and fast meet 1st time.
+        //Distance(head to intersection) = Distance (meet point to intersection)
+        slow = head;
+        fast = fast.next;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+    }
+}
