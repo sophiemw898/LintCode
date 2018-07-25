1beef8ca66b5eada79d403c2367bbbb1e28fde27
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2018-07-25 13:49:37 -0700

    Create 98_Sort List.java

---
+++ 1_Algorithm/9_DataStructure_Interval- Array- Matrix - Binary Indexed Tree/98_Sort List.java
@@ -0,0 +1,53 @@
+//Merge sort
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @return: You should return the head of the sorted linked list, using constant space complexity.
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = findMid(head);
+        
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+        
+        return merge(left, right);
+    }
+    
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        
+        if (head1 == null) {
+            tail.next = head2;
+        } else {
+            tail.next = head1;
+        }
+        
+        return dummy.next;
+    }
+}
