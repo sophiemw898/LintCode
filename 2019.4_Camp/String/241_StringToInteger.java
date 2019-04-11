cd270e9a2c56cdea7a72dfa2a101f61145188a09
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-11 11:44:30 -0700

    Update 241_StringToInteger.java

--- 2019.4_Camp/String/241_StringToInteger.java
+++ 2019.4_Camp/String/241_StringToInteger.java
@@ -1,1 +1,30 @@
-
+public class Solution {
+    /**
+     * @param str: A string
+     * @return: An integer
+     */
+    public int stringToInteger(String str) {
+        // write your code here
+        //return Integer.valueOf(str);
+        
+        int integer = 0;
+        int ifMinus = 0;
+        
+        //check if it is negtive
+        if (str.charAt(0) == '-') {
+            ifMinus = 1;
+        }        
+        
+        //traver all String
+        for (int i = ifMinus; i < str.length(); i++) {
+            integer = 10 * integer + (str.charAt(i) - '0');
+        }
+        
+        //return negtive integer
+        if (ifMinus == 1) {
+            integer = -integer;
+        }
+        
+        return integer;
+    }
+}
