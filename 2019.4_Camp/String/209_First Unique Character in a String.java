6e59c392a0e32d82f2a990699b58a23a63a79c98
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-13 22:00:36 -0700

    Create 209_First Unique Character in a String.java

---
+++ 2019.4_Camp/String/209_First Unique Character in a String.java
@@ -0,0 +1,21 @@
+public class Solution {
+    /**
+     * @param str: str: the given string
+     * @return: char: the first unique character in a given string
+     */
+    public char firstUniqChar(String str) {
+        // Write your code here
+        int[] count = new int[256];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i)]++;
+        }
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (count[str.charAt(i)] == 1) {
+                return str.charAt(i);
+            }
+        }
+        
+        return '0';
+    }
+}
