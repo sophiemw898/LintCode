fca105be02f54683b1caccd3533cf452a8525086
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-15 14:18:28 -0700

    Update 891_Valid Palindrome II.java

--- 2019.4_Camp/String/891_Valid Palindrome II.java
+++ 2019.4_Camp/String/891_Valid Palindrome II.java
@@ -1,3 +1,50 @@
+algorithm 1
+ 
+public class Solution {
+    /**
+     * @param s: a string
+     * @return boolean: whether you can make s a palindrome by deleting at most one character
+     */
+    public boolean validPalindrome(String s) {
+        // Write your code here
+        if (s == null || s.length() <= 1) {
+            return true;
+        }
+        
+        int left = 0, right = s.length() - 1;
+        while(left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+            left++;
+            right--;
+        }
+        
+        //此时已经全部遍历，则s本身就是palindrome
+        if (left >= right) {
+            return true;
+        }
+        
+        return isPalindrome(s, left, right - 1) || isPalindrome(s, left + 1, right);
+    }
+    
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+}
+
+
+
+
+Algorithm 2
 public class Solution {
     /**
      * @param s: a string
