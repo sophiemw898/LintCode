4eda54e1b5c129c685621d0407232c04b7b3c5e6
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-17 10:54:39 -0700

    Update 1183_Single Element in a Sorted Array.java

--- 2019.4_Camp/BinarySearch/1183_Single Element in a Sorted Array.java
+++ 2019.4_Camp/BinarySearch/1183_Single Element in a Sorted Array.java
@@ -7,18 +7,40 @@ public class Solution {
         // write your code here
         //二分找中点mid，取最近的偶数，mid和mid+1相等，则在后半部分start= mid、
         int start = 0, end = nums.length - 1;
+        // while (start + 1 < end) {
+        //     int mid = start + (end - start) / 2;
+        //     //mid向下取偶数 所以后面start = mid + 2 不然会死循环
+        //     mid = (mid%2 == 1) ? mid - 1: mid;
+        //     if (nums[mid] == nums[mid + 1]) {
+        //         start = mid + 2;
+        //     } else {
+        //         end = mid;
+        //     }
+        // }
+        
+        //method2 mid分开写 
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            mid = (mid%2 == 1) ? mid - 1: mid;
-            if (nums[mid] == nums[mid + 1]) {
-                start = mid + 2;
+            if (mid % 2 == 1) {
+                //mid is odd 
+                if (nums[mid] == nums[mid - 1]) {
+                    start = mid;
+                } else { 
+                    end = mid;
+                }
             } else {
-                end = mid;
+                if (nums[mid] == nums[mid + 1]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
             }
         }
-        System.out.println(start);
-        System.out.println(end);
         
-        return nums[start];
+        if (start % 2 == 0) {
+            return nums[start];
+        } else {
+            return nums[end];
+        }
     }
 }
