7500a01e8a3d622d8bd6ca7e44847cf653c69e9c
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-17 15:28:29 -0700

    Create 165_Merge Two Sorted Lists.java

---
+++ 2019.4_Camp/Sort/165_Merge Two Sorted Lists.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param l1: ListNode l1 is the head of the linked list
+     * @param l2: ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // write your code here
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            
+            node = node.next;
+        }
+        
+        if (l1 != null) {
+            node.next = l1;
+        }
+        if (l2 != null) {
+            node.next = l2;
+        }
+        return dummy.next;
+    }
+}
