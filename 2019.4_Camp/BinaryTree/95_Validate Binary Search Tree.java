c37d75f5e928b76e5f470ae0e3d138f4685eb6ab
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-19 12:14:47 -0700

    Create 95_Validate Binary Search Tree.java

---
+++ 2019.4_Camp/BinaryTree/95_Validate Binary Search Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+//Divide & Conquer O(n)
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        // write your code here
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValid(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValid(root.left, min, Math.min(max, root.val)) &&
+                isValid(root.right, Math.max(root.val, min), max);
+    }
+}
