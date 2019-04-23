675d7d7c8c84a635ddee03b34206618e51dc2828
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-23 11:53:00 -0700

    Create 75_Find Peak Element.java

---
+++ 2019.4_Camp/BinarySearch/75_Find Peak Element.java
@@ -0,0 +1,22 @@
+public class Solution {
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+        // write your code here
+        int start = 1, end = A.length - 2;
+        while (start + 1 < end) {
+            int mid = start + (end - start)/2;
+            if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
+                return mid;
+            } else if (A[mid] > A[mid - 1] && A[mid] < A[mid + 1]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return A[start] > A[end] ? start : end;
+    }
+}
