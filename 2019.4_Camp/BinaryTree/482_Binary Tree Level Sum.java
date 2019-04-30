3187aa226b6425162c15b700a46ef9452efd7f75
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-04-30 16:34:08 -0700

    Update 482_Binary Tree Level Sum.java

--- 2019.4_Camp/BinaryTree/482_Binary Tree Level Sum.java
+++ 2019.4_Camp/BinaryTree/482_Binary Tree Level Sum.java
@@ -1,15 +1,31 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
+//DFS
+public class Solution {
+    /**
+     * @param root: the root of the binary tree
+     * @param level: the depth of the target level
+     * @return: An integer
+     */
+    int sum = 0;
+    public int levelSum(TreeNode root, int level) {
+        helper(root, 1, level);
+        return sum;
+    }
+    
+    //calculate the sum when depth == level
+    private void helper(TreeNode root, int depth, int level) {
+        if (root == null) {
+            return;
+        }
+        if (depth == level) {
+            sum += root.val;
+        }
+        
+        helper(root.left, depth + 1, level);
+        helper(root.right, depth + 1, level);
+    }
+}
 
+//Divede & conquer
 public class Solution {
     /**
      * @param root: the root of the binary tree
@@ -31,5 +47,44 @@ public class Solution {
         }
         
         return 0;
+    }
+}
+
+//BFS
+public class Solution {
+    /**
+     * @param root: the root of the binary tree
+     * @param level: the depth of the target level
+     * @return: An integer
+     */
+    public int levelSum(TreeNode root, int level) {
+        // BFS
+        if (level <= 0 || root == null) {
+            return 0;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (level > 1) {
+            level--;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                 
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        
+        int res = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            res += node.val;
+        }
+        return res;
     }
 }
