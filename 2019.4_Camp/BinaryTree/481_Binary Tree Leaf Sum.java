61dbea65cf3b9b8905ddf737159ca6510573cfb7
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-01 12:00:49 -0700

    Create 481_Binary Tree Leaf Sum.java

---
+++ 2019.4_Camp/BinaryTree/481_Binary Tree Leaf Sum.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of the binary tree
+     * @return: An integer
+     */
+    public int leafSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        
+        int left = leafSum(root.left);
+        int right = leafSum(root.right);
+        
+        return left + right;
+    }
+}
