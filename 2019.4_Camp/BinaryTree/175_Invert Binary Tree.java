88036999357393fe54f460fe728ba9ad3bb1880c
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-01 12:20:19 -0700

    Update 175_Invert Binary Tree.java

--- 2019.4_Camp/BinaryTree/175_Invert Binary Tree.java
+++ 2019.4_Camp/BinaryTree/175_Invert Binary Tree.java
@@ -31,3 +31,35 @@ public class Solution {
         return root;
     }
 }
+
+//BFS
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void invertBinaryTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        Queue<TreeNode> queue= new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            
+            TreeNode temp = new TreeNode(0);
+            temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        } 
+    }
+}
