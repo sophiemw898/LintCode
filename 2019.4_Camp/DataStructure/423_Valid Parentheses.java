045d42ecb78ec8104b3c3fdd943613bbc8365013
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-01 14:51:44 -0700

    Create 423_Valid Parentheses.java

---
+++ 2019.4_Camp/DataStructure/423_Valid Parentheses.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /**
+     * @param s: A string
+     * @return: whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+            if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+            if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
