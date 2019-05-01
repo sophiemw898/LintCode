cdd5660af802f459744794d5d7e552daa8a2a527
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-01 11:52:18 -0700

    Update 97_Maximum Depth of Binary Tree.java

--- 2019.4_Camp/BinaryTree/97_Maximum Depth of Binary Tree.java
+++ 2019.4_Camp/BinaryTree/97_Maximum Depth of Binary Tree.java
@@ -27,3 +27,69 @@ public class Solution {
         return Math.max(left, right) + 1;
     }
 }
+
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+    //dfs
+    int res = 0;
+    public int maxDepth(TreeNode root) {
+        helper(root, 1);
+        return res;
+    }
+    
+    private void helper(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+        res = Math.max(res, depth);
+        
+        helper(node.right, depth + 1);
+        helper(node.left, depth + 1);
+    }
+}
+
+//BFS
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+     
+    //bfs
+    public int maxDepth(TreeNode root) {
+        int depth = 0;
+        if (root == null) {
+            return depth;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        //first last记录当前层的首位和尾位
+        int first = 0, last = 1;
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            first++;
+            
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            
+            //when depth++
+            if (first == last) {
+                depth++;
+                first = 0;
+                last = queue.size();
+            }
+        }
+        
+        return depth;
+    }
+}
