34d0c1f91622441e1b7d40fd455c9cd4dde1e48b
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-03 15:07:36 -0700

    Update 589_Connecting Graph.java

--- 2019.4_Camp/UnionFind/589_Connecting Graph.java
+++ 2019.4_Camp/UnionFind/589_Connecting Graph.java
@@ -3,12 +3,22 @@ public class ConnectingGraph {
     * @param n: An integer
     */
     private int[] father = null;
+    //private int[] size = null;
+    
+    private int find (int x) {
+        if (father[x] == x) {
+            return x;
+        }
+        return father[x] = find(father[x]);
+    }
     
     public ConnectingGraph(int n) {
         // do intialization if necessary
         father = new int[n + 1];
+        //size = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             father[i] = i;
+            //size[i] = 1;
         }
     }
 
@@ -22,9 +32,10 @@ public class ConnectingGraph {
         int fatherB = find(b);
         if (fatherB != fatherA) {
             father[fatherA] = fatherB;
+            //size[fatherB] += fatherA;
         }
     }
-
+    
     /*
      * @param a: An integer
      * @param b: An integer
@@ -36,10 +47,9 @@ public class ConnectingGraph {
         return fatherA == fatherB;
     }
     
-    private int find (int x) {
-        if (father[x] == x) {
-            return x;
-        }
-        return father[x] = find(father[x]);
-    }
+    /*//caculate the size of each union
+    private int query2(int a) {
+        return size[a];
+    }    
+    */
 }
