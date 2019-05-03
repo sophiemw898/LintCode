8d87f01c512e7bba3d048da2faee3b7cd1739741
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-02 17:27:22 -0700

    Create 1054_Min Cost Climbing Stairs.java

---
+++ 2019.4_Camp/DP/1054_Min Cost Climbing Stairs.java
@@ -0,0 +1,18 @@
+public class Solution {
+    /**
+     * @param cost: an array
+     * @return: minimum cost to reach the top of the floor
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        
+        dp[0] = 0;
+        dp[1] = 0;
+        
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min((dp[i - 2] + cost[i - 2]), (dp[i - 1] + cost[i - 1]));
+        }
+        
+        return dp[cost.length];
+    }
+}
