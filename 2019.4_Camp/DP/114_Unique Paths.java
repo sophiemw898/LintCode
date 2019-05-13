1139c780bfaa5b4ae5ad3ebd744c4511eb0b4291
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-13 14:53:59 -0700

    Create 114_Unique Paths.java

---
+++ 2019.4_Camp/DP/114_Unique Paths.java
@@ -0,0 +1,29 @@
+public class Solution {
+    /**
+     * @param m: positive integer (1 <= m <= 100)
+     * @param n: positive integer (1 <= n <= 100)
+     * @return: An integer
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] =  dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
