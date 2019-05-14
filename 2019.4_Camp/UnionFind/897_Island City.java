b87e84cf373aa35c0d89a7e2d837215d4222b8b7
sophiemw898 <31332241+sophiemw898@users.noreply.github.com>
2019-05-14 13:35:55 -0700

    Update 897_Island City.java

--- 2019.4_Camp/UnionFind/897_Island City.java
+++ 2019.4_Camp/UnionFind/897_Island City.java
@@ -1,3 +1,75 @@
+//BFS
+public class Solution {
+    /**
+     * @param grid: an integer matrix
+     * @return: an integer 
+     */
+    private int SEA = 0;
+    private int CITY = 2;
+    private int ISLAND = 1;
+    private int[] dirs = {-1, 0, 1, 0, -1};
+    
+    public int numIslandCities(int[][] grid) {
+        
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == SEA) {
+                    continue;
+                } 
+                if (bfs(grid, i ,j)) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private boolean bfs(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length; 
+        boolean hasCity = false;
+        
+        if (grid[i][j] == CITY) {
+            hasCity = true;
+        }
+        
+        grid[i][j] = SEA;//mark as visited;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(i * n + j);
+        
+        while(!queue.isEmpty()) {
+            
+            int cur = queue.poll();
+            
+            for (int index = 0; index < 4; index++) {
+                int X = cur / n + dirs[index];
+                int Y = cur % n + dirs[index + 1];
+                if (X < 0 || X >= m || Y < 0 || Y >= n || grid[X][Y] == SEA) {
+                    continue;
+                }
+                
+                if (grid[X][Y] == CITY) {
+                    hasCity = true;
+                }
+                
+                grid[X][Y] = SEA;
+                queue.offer(X * n + Y);
+            }
+        }
+        
+        return hasCity;
+    }
+}
+
 //DFS
 public class Solution {
     /**
